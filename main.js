d19dcd1d958fbc78f549ef43e4506434a5e19f43
Absozero <aswsub123@gmail.com>
2022-06-24 17:50:34 +0530

    update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+const electron = require('electron');
+
+const url = require('url');
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
